089bbb142ec85a03ea3be395cc8e370cf21a1a4c
masaki.yamane <user088.dounyu2026@tmns-3ssscom.net>
2026-05-13 13:46:47 +0900

    パスワード変更完了

--- src/main/java/util/DBUtil.java
+++ src/main/java/util/DBUtil.java
@@ -11,8 +11,8 @@ public class DBUtil {
     private static final String USER =
             "root";
 
-    private static final String PASSWORD =
-            "marinL613399";
+    private static final String PASSWORD = 
+            "masaki";
 
     public static Connection getConnection()
             throws Exception {
