1993eddfec52a7f9dfb58a1481875d21e2764841
User <User@SYSMDS1071>
2026-05-13 14:08:59 +0900

    よろ

--- src/main/java/util/DBUtil.java
+++ src/main/java/util/DBUtil.java
@@ -11,8 +11,8 @@ public class DBUtil {
     private static final String USER =
             "root";
 
-    private static final String PASSWORD = 
-            "masaki";
+    private static final String PASSWORD =
+            "marinL613399";
 
     public static Connection getConnection()
             throws Exception {
